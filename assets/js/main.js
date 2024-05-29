8b4d6c374bb6025f53f5b9159f3c3b8c9ce41839
Varkkichan <ajayaji.383@gmail.com>
2024-05-30 00:54:05 +0530

    updated with javascript files that helps to toggle the arrow

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,6 +12,27 @@
 /*=============== SKILLS TABS ===============*/
 
 
+           
+            const tabs = document.querySelectorAll('[data-target]');
+            const tabContent = document.querySelectorAll('[data-content]');
+        
+            tabs.forEach(tab => {
+                tab.addEventListener('click', () => {
+                    const target = document.querySelector(tab.dataset.target);
+        
+                    tabContent.forEach(content => {
+                        content.classList.remove('skills_active');
+                    });
+                    target.classList.add('skills_active');
+        
+                    tabs.forEach(tab => {
+                        tab.classList.remove('skills_active');
+                    });
+                    tab.classList.add('skills_active');
+                });
+            });
+       
+
 /*=============== MIXITUP FILTER PORTFOLIO ===============*/
 
 
